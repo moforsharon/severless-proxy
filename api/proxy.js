ce3a8dd1938aa4f26292e16e354f9e7b5624011b
moforsharon <moforsharon55@gmail.com>
2024-09-05 16:03:32 +0100

    proxy sever

--- api/proxy.js
+++ api/proxy.js
@@ -5,7 +5,7 @@ import { v4 as uuidv4 } from 'uuid';
 export default async function handler(req, res) {
   // Set CORS headers for all requests
   res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Origin', '*'); // Allow your specific origin
+  res.setHeader('Access-Control-Allow-Origin', 'https://cbc-one.vercel.app'); // Allow your specific origin
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
   res.setHeader(
     'Access-Control-Allow-Headers',
@@ -92,7 +92,7 @@ export default async function handler(req, res) {
 
     // Set CORS headers for the actual response
     res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Replace with specific origin
+    res.setHeader('Access-Control-Allow-Origin', 'https://cbc-one.vercel.app'); // Replace with specific origin
     res.setHeader(
       'Access-Control-Allow-Headers',
       'X-CSRF-Token, X-Requested-With, Authorization, Content-Type, Accept, Origin'
@@ -102,7 +102,7 @@ export default async function handler(req, res) {
   } catch (error) {
     // Set CORS headers for errors as well
     res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Replace with specific origin
+    res.setHeader('Access-Control-Allow-Origin', 'https://cbc-one.vercel.app'); // Replace with specific origin
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
     res.setHeader(
       'Access-Control-Allow-Headers',
